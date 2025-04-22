489ea9a7772d2ddacafed1266bafcc61218c48d1
batmhannJ <reyeshannahjoy82@gmail.com>
2025-04-22 13:32:56 +0800

    My local flutter changes

--- ridermobile/android/app/src/main/kotlin/com/fraved/restaurant/MainActivity.kt
+++ ridermobile/android/app/src/main/kotlin/com/fraved/restaurant/MainActivity.kt
@@ -1,13 +1,5 @@
 package com.fraved.restaurant
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import com.baseflow.permissionhandler.PermissionHandlerPlugin
 
-class MainActivity: FlutterActivity() {
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        println("Registering PermissionHandlerPlugin...")
-        flutterEngine.plugins.add(PermissionHandlerPlugin())
-    }
-}
+class MainActivity: FlutterActivity()
